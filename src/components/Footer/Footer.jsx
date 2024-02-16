be1997fd0b20579cc24da11286743c63d3cca3b1
Isaura da Hora <146293517+isauradahora@users.noreply.github.com>
2024-02-15 23:58:38 -0300

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import styles from './footer.module.css'
+
+
+function Footer() {
+  return(
+    <footer className={styles.footer}>
+      <b>
+       Desenvolvido por Isaura da Hora 🔮
+      </b>
+    </footer>
+    
+  )
+}
+
+export default Footer
