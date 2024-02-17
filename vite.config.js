2beac5fab7460f0284e6e1af423861def1fec0f3
Isaura da Hora <146293517+isauradahora@users.noreply.github.com>
2024-02-17 20:02:40 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom']
     }
   }
 })
